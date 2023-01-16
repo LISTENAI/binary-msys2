e5a4a224c89039dcf0b82a0d2dc138da9060d695
seasonyuu <seasonyuu@gmail.com>
2023-01-16 17:47:45 +0800

    update binary dir

--- src/index.ts
+++ src/index.ts
@@ -7,7 +7,7 @@ export const INSTALLER_VERSION = "2022-03-19"
 export default <Binary>{
   homeDir: HOME,
 
-  binaryDir: HOME,
+  binaryDir: join(HOME, "msys2", "msys64", "usr", "bin"),
 
   async version() {
     return INSTALLER_VERSION;
